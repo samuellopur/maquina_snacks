ae16822ad25bb6b2e83105920f26e92ca1eecbce
Samuel Lopez <samuellopez01@hotmail.com>
2024-10-21 09:45:57 -0500

    Clase Snacks para crear lista

---
+++ src/Snacks.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snacks {
+    private static final List<Snack> snacks;
+
+//    Bloque static inicializador
+    static {
+        snacks = new ArrayList<>();
+        snacks.add(new Snack("Papas", 70.0));
+        snacks.add(new Snack("Refresco", 50.0));
+        snacks.add(new Snack("Queso", 120.0));
+    }
+
+    public static void agregarElemento(Snack snack){
+        snacks.add(snack);
+    }
+
+    public static void mostrarSnacks(){
+        var inventarioSnacks = "";
+        for (var snack: snacks){
+            inventarioSnacks += snack.toString() + "\n";
+            System.out.println("""
+                    -------------------------------
+                    --- Snacks en el Inventario ---
+                    -------------------------------""");
+            System.out.println(inventarioSnacks);
+        }
+    }
+
+    public static List<Snack> getSnacks(){
+        return snacks;
+    }
+}
